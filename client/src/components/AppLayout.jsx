b2fd2c5f86a87347edffa046904fd363acd31b64
chloemk <minsung.chloe.kim@gmail.com>
2021-09-08 21:59:53 +0900

    temporary commit

--- client/src/components/AppLayout.jsx
+++ client/src/components/AppLayout.jsx
@@ -3,10 +3,14 @@ import React from 'react';
 const Header = () => {
   return (
     <>
-      {/* sticky */}
-      {/* 스크롤 애니메이션 (PAFFEM) */}
-      {/* 햄버거 애니메이션 (PAFFEM) */}
-      {/* Logo 왼쪽 */}
+      {/* 헤더는 sticky로 스크롤해도 고정한다. */}
+      {/* 키보드, 타건샵 메뉴 왼쪽 위치 */}
+      {/* 스크롤 애니메이션 (PAFFEM)처럼 스크롤하다가 헤더에서 메인으로 내려오는 순간 헤더 색 변경 */}
+      {/* 반응형으로 왼쪽에 햄버거 애니메이션 (PAFFEM) */}
+      {/* Logo 중간 */}
+      {/* isLogin이 false면 오른쪽에 사람 아이콘 하나 */}
+      {/* isLogin이 true면 사람 아이콘 + 나가기 아이콘 */}
+      {/* isLogin이 true면 사람아이콘 클릭 시, 마이페이지 & false일 경우엔 로그인 페이지로 이동 */}
     </>
   );
 };
@@ -14,7 +18,14 @@ const Main = () => {
   return <></>;
 };
 const Footer = () => {
-  return <></>;
+  return (
+    <>
+      {/* 왼쪽: 로고 회색깔로 넣기 */}
+      {/* 오른쪽: 문의: 1:1문의 */}
+      {/* 오른쪽: 서비스 소개: REPOSITORY & WIKI*/}
+      {/* 오른쪽: Team Members: 정선아, 김민성, 백승문, 박준호 */}
+    </>
+  );
 };
 
 const AppLayout = ({ children }) => {
@@ -22,8 +33,8 @@ const AppLayout = ({ children }) => {
     <>
       {/* <Header /> */}
       {/* <Main>{children}</Main> */}
-      {/* <Footer /> */}
       {children}
+      {/* <Footer /> */}
     </>
   );
 };
--- client/src/pages/Survey.jsx
+++ client/src/pages/Survey.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 
 const Survey = () => {
-  return <></>;
+  //TODO: API 하나 더 만들어달라고 요청해야함
+  //FIXME: 게이밍, 노이즈, 사운드 등등 useState로 만듬
+  //클릭핸들러 함수
+  return (
+    <>
+      {/* section 태그로 된 큰 테스트 카드 만들기 */}
+      {/* 시작하기 버튼 transparent로 만들기 */}
+      {/* 누를 때마다 그에 맞는 답을 우리가 useState에 저장한다. */}
+      {/*
+        if(!isStarted){
+          return <start 버튼 있는 컴포넌트 />
+        else {
+          if(gaming === null) return <Question1 onClickAnswer1 = {onClickAnswer1} />
+          else if(!noise) return <Question2 />
+          else if(!weight) return <Question3 />
+          else if(!sound) return <Question4 />
+        }
+        
+        } 
+      */}
+      1. 어떤 용도 (게임, 개발, 사무) 용도 2. 소음 (조용, 보통, 시끄러워도 상관
+      없다) 3. 키압(키를 누를 때 줘야 하는 힘의 크기)(무거운게 좋다, 가벼운게 4.
+      타건감 (도각(짧게 들어보기), 서걱(짧게 들어보기 버튼), 보글, 찰칵) 5.
+      로딩중... 6. 당신에게 맞는 키보드는..? 7. 짜잔
+    </>
+  );
 };
 
 export default Survey;
