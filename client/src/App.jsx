8563470f2827da13876463257944c75967032fd5
seona-jung <seona.jung.job@gmail.com>
2021-09-09 19:41:44 +0900

    Update: App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import Keyboard from './pages/Keyboard';
-import Landing from './pages/Landing';
+// import Landing from './pages/Landing';
 import Survey from './pages/Survey';
 
 function App() {
