011b7ba2c10ca800c743161213845055208e0f3f
seona-jung <seona.jung.job@gmail.com>
2021-09-09 22:57:12 +0900

    Test cookie

--- client/src/pages/Keyboard.jsx
+++ client/src/pages/Keyboard.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+axios.defaults.withCredentials = true;
 
 import AppLayout from '../components/AppLayout';
 
 const Keyboard = () => {
+  useEffect(async () => {
+    try {
+      const token = await axios.get(
+        `${process.env.REACT_APP_API_URL}/users/checkToken`
+      );
+      console.log(token.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
     <>
       <AppLayout>
