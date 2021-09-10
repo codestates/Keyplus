ac6be539c59a8606595468daa3b9f21ab02debef
do8972 <do8972@gmail.com>
2021-09-10 13:52:18 +0900

    Add: feature/keyboards-api

--- server/controller/keyboards.js
+++ server/controller/keyboards.js
@@ -4,9 +4,29 @@ const { User, Keyboard } = require('../models');
 module.exports = {
   getAllKeyboards: async (req, res) => {
     // 1. 업로드되어있는 모든 키보드를 조회해서 클라이언트로 보내준다. ( findAll )
+    try {
+      const getAllKeyboard = await Keyboard.findAll({
+        raw: true,
+      });
+      return res.status(200).json({ data: getAllKeyboard });
+    } catch (error) {
+      return res.status(500).json({ message : "Server Error" });
+    };
   },
   getKeyboardsById: async (req, res) => {
-    // 1. params 로 아이디를 받아온다.
-    // 2. 바아온 아이디로 특정 키보드를 조회한 후 클라이언트로 보내준다.
+    try {
+      // 1. params 로 keyboardId를 받아온다.
+      const keyboardId = req.params.id
+      // 2. 바아온 아이디로 특정 키보드를 조회한 후 클라이언트로 보내준다.
+      const getKeyboardId = await Keyboard.findOne({
+        where: {
+          id: keyboardId
+        },
+        raw: true,
+      })
+      return res.status(200).json({ data: getKeyboardId });
+    } catch (error) {
+      return res.status(500).json({ message : "Server Error" });
+    }
   },
 };
