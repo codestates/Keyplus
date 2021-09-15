e86f078b21913c2bb15bc0bf101c185a58a4a814
seona-jung <seona.jung.job@gmail.com>
2021-09-15 21:05:08 +0900

    Update: start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,9 +7,19 @@ export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
 
-export JWT_SECRETKEY=$(aws ssm get-parameters --region ap-northeast-2 --names JWT_SECRETKEY --query Parameters[0].Value | sed 's/"//g')
-export EXPIRES_DAY=$(aws ssm get-parameters --region ap-northeast-2 --names EXPIRES_DAY --query Parameters[0].Value | sed 's/"//g')
-export SALT_ROUNDS=$(aws ssm get-parameters --region ap-northeast-2 --names SALT_ROUNDS --query Parameters[0].Value | sed 's/"//g')
-
+export MAILID=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export MAILPW=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export S3_BUCKET_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export S3_KEY_ID=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export S3_REGION=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_CLIENT_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_REDIRECT_URI=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export NAVER_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export NAVER_CLIENT_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export NAVER_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export NAVER_REDIRECT_URI=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export KAKAO_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export KAKAO_REDIRECT_URI=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start app.js
