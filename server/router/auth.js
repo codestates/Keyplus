09fc5e003366b9ba9528ec57685442cb620f4651
goodbsm2421 <goodbsm2421@gmail.com>
2021-09-15 16:52:46 +0900

    Add: OAuth

--- server/router/auth.js
+++ server/router/auth.js
@@ -8,8 +8,11 @@ const router = express.Router();
 router.post('/login', authController.login); // login API
 router.post('/logout', isAuth, authController.logout); // logout API
 router.post('/signup', uploads.single('img'), authController.signup); // singup API
-router.post('/google', authController.googleLogin); // 구글 오어스
-router.post('/naver', authController.naverLogin); // 네이버 오어스
-router.post('/kakao', authController.kakaoLogin); // 카카오 오어스
+router.get('/google', authController.googleLogin); // 구글 오어스
+router.get('/googleCallback', authController.googleCallback);
+router.get('/naver', authController.naverLogin); // 네이버 오어스
+router.get('/naverCallback', authController.naverCallback);
+router.get('/kakao', authController.kakaoLogin); // 카카오 오어스
+router.get('/kakaoCallback', authController.kakaoCallback);
 
 module.exports = router;
