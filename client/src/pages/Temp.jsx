709ba192a720b5f38f8ecf8180ea997b428d88de
seona-jung <seona.jung.job@gmail.com>
2021-09-16 14:08:24 +0900

    Update: OAuth Login

--- client/src/pages/Temp.jsx
+++ client/src/pages/Temp.jsx
@@ -1,25 +1,19 @@
 import React, { useEffect } from 'react';
-import cookies from 'js-cookies';
+import { useDispatch } from 'react-redux';
+import { socialLogIn } from '../reducers/api/userAPI';
+import { isError } from '../reducers/errorReducer';
 
 const Temp = ({ history }) => {
-  useEffect(() => {
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());
-    const accessToken = params.accessToken;
-    console.log(accessToken);
-    //  axios
-    //   .get(`${process.env.REACT_APP_API_URL}/users`, {
-    //     headers: {
-    //       Authorization: `Bearer ${accessToken}`,
-    //     },
-    //   })
-    //   .then((res) => {
-    //     localStorage.setItem('accessToken', accessToken);
-    //     dispatch(setLoginModal(false));
-    //     dispatch(setIsLogin(true));
-    //     dispatch(setUserinfo(res.data.user));
-    //   });
-    history.push('/keyboards');
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    try {
+      await dispatch(socialLogIn()).unwrap();
+    } catch (err) {
+      console.log(err);
+      dispatch(isError(err.response));
+    }
+    history.push('/');
   }, []);
   return <></>;
 };
