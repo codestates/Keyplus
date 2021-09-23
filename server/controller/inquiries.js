5d7860d8b9f6b7bed7086b268882e0a640186847
seona-jung <seona.jung.job@gmail.com>
2021-09-23 22:01:25 +0900

    Update: inquiry content

--- server/controller/inquiries.js
+++ server/controller/inquiries.js
@@ -8,8 +8,8 @@ module.exports = {
     // 1. 문의를 작성한 유저 이메일과 content 를 클라이언트로부터 받아온다.
     // 2. nodemailer 를 사용해서 admin 메일 계정으로 보낸다.
     try {
-      const { title, email, content } = req.body
-      console.log(title, email, content)
+      const { title, category, email, name, content } = req.body;
+      console.log(title, email, content);
 
       const transporter = nodemailer.createTransport({
         service: 'Naver',
@@ -25,10 +25,14 @@ module.exports = {
         to: process.env.MAILID,
         subject: title,
         text: content,
-        html: `<p><b>Title</b>: ${title}</p>
-        <p><b>To</b>: ${email}</p>
-        <p><b>content</b>: ${content}</p>`
-      }
+        html: `
+        <p><b>제목</b>: ${title}</p>
+        <p><b>문의 분류</b>: ${category}</p>
+        <p><b>작성자 이메일</b>: ${email}</p>
+        <p><b>작성자 이름</b>: ${name}</p>
+        <p><b>문의 내용</b>: ${content}</p>
+        `,
+      };
       transporter.sendMail(message, function (err, info) {
         if (err) {
           console.log(err);
