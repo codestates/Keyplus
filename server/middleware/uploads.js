90191ac73755a651d61a34cf6a2ddf925ae9b545
seona-jung <seona.jung.job@gmail.com>
2021-09-27 22:30:35 +0900

    Update: js syntax

--- server/middleware/uploads.js
+++ server/middleware/uploads.js
@@ -22,13 +22,13 @@ const storage = multerS3({
   key: function (req, file, cb) {
     if (file.originalname.match(/\.(mp4|MPEG-4|mkv)$/)) {
       cb(null, `reviewVideo/${Date.now()}_${file.originalname}`);
-    } else if (req.files?.keyboardImg) {
+    } else if (req.files && req.files.keyboardImg) {
       cb(null, `keyboards/${Date.now()}_${file.originalname}`);
-    } else if (req.files?.img1) {
+    } else if (req.files && req.files.img1) {
       cb(null, `review/${Date.now()}_${file.originalname}`);
-    } else if (req.files?.img2) {
+    } else if (req.files && req.files.img2) {
       cb(null, `review/${Date.now()}_${file.originalname}`);
-    } else if (req.files?.img3) {
+    } else if (req.files && req.files.img3) {
       cb(null, `review/${Date.now()}_${file.originalname}`);
     } else {
       cb(null, `profile/${Date.now()}_${file.originalname}`);
