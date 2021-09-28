eaa50ab0cb6291c26f0acef45d19068c394e8805
seona-jung <seona.jung.job@gmail.com>
2021-09-28 20:45:47 +0900

    Update: bucket name

--- server/middleware/uploads.js
+++ server/middleware/uploads.js
@@ -2,7 +2,6 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const AWS = require('aws-sdk');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
 const s3 = new AWS.S3({
@@ -13,8 +12,7 @@ const s3 = new AWS.S3({
 
 const storage = multerS3({
   s3: s3,
-  // bucket: process.env.S3_BUCKET_NAME,
-  bucket: 'keyplus-s3-image',
+  bucket: process.env.S3_BUCKET_NAME,
   contentType: multerS3.AUTO_CONTENT_TYPE,
   acl: 'public-read',
   metadata: function (req, file, cb) {
