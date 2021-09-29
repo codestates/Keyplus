6b4661499c1ffce62205e6029918457b76e1436d
seona-jung <seona.jung.job@gmail.com>
2021-09-30 00:28:33 +0900

    Fix: a typo

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import LandingHeader from '../components/LandingHeader';
 import LandingPart1 from '../components/Landing/LandingPart1';
