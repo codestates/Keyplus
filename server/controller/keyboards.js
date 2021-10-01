0e167177c43c57dbce829d7e00558dcb45e35da9
do8972 <do8972@gmail.com>
2021-10-01 12:40:33 +0900

    Fix: key option

--- server/controller/keyboards.js
+++ server/controller/keyboards.js
@@ -30,7 +30,7 @@ module.exports = {
   getFilteredKeyboards: async (req, res) => {
     try {
       const { color, backlight, tenkey, bluetooth, price } = req.body;
-      const keys = { 저소음적축: 1, 적축: 1, 갈축: 2, 흑축: 3, 청축: 4 };
+      const keys = { 저소음적축: 1, 갈축: 2, 흑축: 3, 적축: 3, 청축: 4 };
       let getSwitch = {};
       for (let key in keys) {
         // 반복문을 돌려 req.body.switch를 getKeys에 넣는다.
