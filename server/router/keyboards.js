c2651bf5ca98cb816cde7d32d903f936f299a7df
do8972 <do8972@gmail.com>
2021-10-01 14:12:59 +0900

    Fix: filter

--- server/router/keyboards.js
+++ server/router/keyboards.js
@@ -4,7 +4,7 @@ const keyboardsController = require('../controller/keyboards');
 const router = express.Router();
 
 router.get('/', keyboardsController.getAllKeyboards); // 모든 키보드리스트 조회
-router.get('/filter', keyboardsController.getFilteredKeyboards); // 필터는 통한 키보드 조회
+router.post('/filter', keyboardsController.getFilteredKeyboards); // 필터는 통한 키보드 조회
 router.get('/:id', keyboardsController.getKeyboardsById); // 특정 키보드 상세페이지 조회
 
 module.exports = router;
