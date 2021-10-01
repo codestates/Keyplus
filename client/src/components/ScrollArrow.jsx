59e3202d0c10873db9bf35900afd82e179121cf9
seona-jung <seona.jung.job@gmail.com>
2021-10-01 21:59:28 +0900

    Add: scroll to top button

---
+++ client/src/components/ScrollArrow.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { FaArrowCircleUp } from 'react-icons/fa';
+
+import './styles/ScrollArrow.scss';
+
+const ScrollArrow = ({ landing }) => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  window.addEventListener('scroll', checkScrollTop);
+
+  return (
+    <FaArrowCircleUp
+      className="scrollTop"
+      onClick={scrollTop}
+      style={{
+        color: landing ? '#fff' : '#000',
+        display: showScroll ? 'flex' : 'none',
+      }}
+    />
+  );
+};
+
+export default ScrollArrow;
