791a041cc8fe10ec99370b18a0c9c5becb55c377
chloemk <minsung.chloe.kim@gmail.com>
2021-10-02 15:21:50 +0900

    Add: survey sound effect added

--- client/src/components/Survey/Question1.jsx
+++ client/src/components/Survey/Question1.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useWidthSize from '../../hooks/useWidthSize';
+import { FiPlayCircle } from 'react-icons/fi';
 
 import '../styles/QuestionCard.scss';
 
-const Question1 = ({ onClickSound }) => {
-  // const audio1 = new Audio('/click.mp3');
-  // const audio2 = new Audio('/click.mp3');
-  // const audio3 = new Audio('/click.mp3');
-
+const Question1 = ({ onClickSound, audio1, audio2, audio3, audio4 }) => {
   const width = useWidthSize(768);
 
-  const audio4 = new Audio('/click.mp3');
+  const [audioOne, setAudioOne] = useState(false);
 
-  const playAudio = () => {
-    audio4.play();
+  const audio = (num) => {
+    if (audioOne) {
+      if (num === 1) {
+        audio1.play();
+      } else if (num === 2) {
+        audio2.play();
+      } else if (num === 3) {
+        audio3.play();
+      } else if (num === 4) {
+        audio4.play();
+      }
+    } else if (!audioOne) {
+      if (num === 1) {
+        audio1.pause();
+      } else if (num === 2) {
+        audio2.pause();
+      } else if (num === 3) {
+        audio3.pause();
+      } else if (num === 4) {
+        audio4.pause();
+      }
+    }
   };
-
-  const stopAudio = () => {
-    audio4.pause();
-  };
+  console.log(audioOne);
 
   return (
     <>
@@ -30,51 +44,135 @@ const Question1 = ({ onClickSound }) => {
           <div className="question-description">
             이미지에 마우스를 올리면 소리가 재생됩니다.
           </div>
-        ) : null}
+        ) : (
+          <div className="question-description">
+            버튼 클릭 시 소리가 재생됩니다.
+          </div>
+        )}
       </div>
       <div className="card-container question1">
-        <div className="card" onClick={() => onClickSound(1)}>
+        <div className="card">
           <div className="image-wrapper">
-            <img src="/no-image.png" alt="" />
+            <img
+              src="/soup.png"
+              onClick={() => onClickSound(1)}
+              onMouseEnter={() => {
+                setAudioOne(false);
+                audio(1);
+              }}
+              onMouseLeave={() => {
+                setAudioOne(true);
+                audio(1);
+              }}
+            />
           </div>
           <div className="text-wrapper">
             <div className="description">찌개 끓는 소리</div>
-            <div className="title">보글보글</div>
+            <div className="title">
+              보글보글
+              <div
+                className="mp3-icon"
+                onClick={() => {
+                  setAudioOne((audioOne) => !audioOne);
+                  audio(1);
+                }}
+              >
+                {width < 768 && <FiPlayCircle />}
+              </div>
+            </div>
           </div>
         </div>
-        <div className="card" onClick={() => onClickSound(2)}>
+        <div className="card">
           <div className="image-wrapper">
-            <img src="/no-image.png" alt="" />
+            <img
+              src="/chocolate.png"
+              onClick={() => onClickSound(2)}
+              onMouseEnter={() => {
+                setAudioOne(false);
+                audio(2);
+              }}
+              onMouseLeave={() => {
+                setAudioOne(true);
+                audio(2);
+              }}
+            />
           </div>
           <div className="text-wrapper">
             <div className="description">초콜릿 부러뜨리는 소리</div>
             <div className="title">
               도각도각
-              <div className="mp3-icon">{width > 768 ? null : '띡'}</div>
+              <div
+                className="mp3-icon"
+                onClick={() => {
+                  setAudioOne((audioOne) => !audioOne);
+                  audio(2);
+                }}
+              >
+                {width < 768 && <FiPlayCircle />}
+              </div>
             </div>
           </div>
         </div>
-        <div className="card" onClick={() => onClickSound(3)}>
+        <div className="card">
           <div className="image-wrapper">
-            <img src="/no-image.png" alt="" />
+            <img
+              src="/pen.png"
+              onClick={() => onClickSound(3)}
+              onMouseEnter={() => {
+                setAudioOne(false);
+                audio(3);
+              }}
+              onMouseLeave={() => {
+                setAudioOne(true);
+                audio(3);
+              }}
+            />
           </div>
           <div className="text-wrapper">
-            <div className="description">서걱의 정체는 무엇인가?</div>
-            <div className="title">서걱서걱 </div>
+            <div className="description">만년필로 글씨쓰는 소리</div>
+            <div className="title">
+              서걱서걱
+              <div
+                className="mp3-icon"
+                onClick={() => {
+                  setAudioOne((audioOne) => !audioOne);
+                  audio(3);
+                }}
+              >
+                {width < 768 && <FiPlayCircle />}
+              </div>
+            </div>
           </div>
         </div>
-        <div
-          className="card"
-          onClick={() => onClickSound(4)}
-          onMouseEnter={() => playAudio()}
-          onMouseLeave={() => stopAudio()}
-        >
+        <div className="card">
           <div className="image-wrapper">
-            <img src="/gaming.png" alt="" />
+            <img
+              src="/gaming.png"
+              onClick={() => onClickSound(4)}
+              onMouseEnter={() => {
+                setAudioOne(false);
+                audio(4);
+              }}
+              onMouseLeave={() => {
+                setAudioOne(true);
+                audio(4);
+              }}
+            />
           </div>
           <div className="text-wrapper">
             <div className="description">PC방 키보드 소리</div>
-            <div className="title">찰칵찰칵</div>
+            <div className="title">
+              찰칵찰칵
+              <div
+                className="mp3-icon"
+                onClick={() => {
+                  setAudioOne((audioOne) => !audioOne);
+                  audio(4);
+                }}
+              >
+                {width < 768 && <FiPlayCircle />}
+              </div>
+            </div>
           </div>
         </div>
       </div>
