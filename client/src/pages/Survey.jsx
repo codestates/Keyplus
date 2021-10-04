b64003606469154ffb6d012976e80e5341c3e1d4
seona-jung <seona.jung.job@gmail.com>
2021-10-04 21:49:08 +0900

    Fix: sound bug on mobile

--- client/src/pages/Survey.jsx
+++ client/src/pages/Survey.jsx
@@ -33,6 +33,11 @@ const delay = () => {
   });
 };
 
+const Audio1 = new Audio('/boggle.mp3');
+const Audio2 = new Audio('/nonclick.mp3');
+const Audio3 = new Audio('/linear.mp3');
+const Audio4 = new Audio('/click.mp3');
+
 const Survey = () => {
   const history = useHistory();
 
@@ -79,24 +84,24 @@ const Survey = () => {
       : null
   );
 
-  const [audio1, setAudio1] = useState(new Audio('/boggle.mp3'));
-  const [audio2, setAudio2] = useState(new Audio('/nonclick.mp3'));
-  const [audio3, setAudio3] = useState(new Audio('/linear.mp3'));
-  const [audio4, setAudio4] = useState(new Audio('/click.mp3'));
+  const audio1 = useRef(Audio1);
+  const audio2 = useRef(Audio2);
+  const audio3 = useRef(Audio3);
+  const audio4 = useRef(Audio4);
 
   //! useEffect
   useEffect(() => {
     console.log('Survey 컴포넌트가 화면에 나타남');
     console.log('started', isStarted);
     return () => {
-      audio1.pause();
-      audio1.currentTime = 0;
-      audio2.pause();
-      audio2.currentTime = 0;
-      audio3.pause();
-      audio3.currentTime = 0;
-      audio4.pause();
-      audio4.currentTime = 0;
+      audio1.current.pause();
+      audio1.current.currentTime = 0;
+      audio2.current.pause();
+      audio2.current.currentTime = 0;
+      audio3.current.pause();
+      audio3.current.currentTime = 0;
+      audio4.current.pause();
+      audio4.current.currentTime = 0;
       console.log('Audio를 꺼라.');
       console.log('Survey 컴포넌트가 화면에서 사라짐');
       console.log('started', isStarted);
@@ -105,17 +110,17 @@ const Survey = () => {
 
   // useEffect(() => {
   //   return () => {
-  //     audio1.pause();
-  //     audio1.currentTime = 0;
-  //     audio2.pause();
-  //     audio2.currentTime = 0;
-  //     audio3.pause();
-  //     audio3.currentTime = 0;
-  //     audio4.pause();
-  //     audio4.currentTime = 0;
+  //     audio1.current.pause();
+  //     audio1.current.currentTime = 0;
+  //     audio2.current.pause();
+  //     audio2.current.currentTime = 0;
+  //     audio3.current.pause();
+  //     audio3.current.currentTime = 0;
+  //     audio4.current.pause();
+  //     audio4.current.currentTime = 0;
   //     console.log('Audio를 꺼라.');
   //   };
-  // }, [audio1, audio2, audio3, audio4]);
+  // }, [audio1.current, audio2.current, audio3.current, audio4.current]);
 
   const mountedSound = useRef(false);
   useEffect(() => {
@@ -213,38 +218,38 @@ const Survey = () => {
   //     return;
   //   } else {
   //     return () => {
-  //       console.log('여긴 audio1,2,3,4 반응, 100');
-  //       console.log(audio1, audio2, audio3, audio4);
+  //       console.log('여긴 audio1.current,2,3,4 반응, 100');
+  //       console.log(audio1.current, audio2.current, audio3.current, audio4.current);
 
-  //       audio1.pause();
-  //       audio1.currentTime = 0;
-  //       audio2.pause();
-  //       audio2.currentTime = 0;
-  //       audio3.pause();
-  //       audio3.currentTime = 0;
-  //       audio4.pause();
-  //       audio4.currentTime = 0;
+  //       audio1.current.pause();
+  //       audio1.current.currentTime = 0;
+  //       audio2.current.pause();
+  //       audio2.current.currentTime = 0;
+  //       audio3.current.pause();
+  //       audio3.current.currentTime = 0;
+  //       audio4.current.pause();
+  //       audio4.current.currentTime = 0;
   //     };
   //   }
-  // }, [audio1, audio2, audio3, audio4]);
+  // }, [audio1.current, audio2.current, audio3.current, audio4.current]);
 
   const onClickSound = useCallback(
     (res) => {
       setSound(res);
 
-      audio1.pause();
-      audio1.currentTime = 0;
+      audio1.current.pause();
+      audio1.current.currentTime = 0;
 
-      audio2.pause();
-      audio2.currentTime = 0;
+      audio2.current.pause();
+      audio2.current.currentTime = 0;
 
-      audio3.pause();
-      audio3.currentTime = 0;
+      audio3.current.pause();
+      audio3.current.currentTime = 0;
 
-      audio4.pause();
-      audio4.currentTime = 0;
+      audio4.current.pause();
+      audio4.current.currentTime = 0;
     },
-    [audio1, audio2, audio3, audio4]
+    [audio1.current, audio2.current, audio3.current, audio4.current]
   );
 
   const convertSoundToText = useCallback((sound) => {
